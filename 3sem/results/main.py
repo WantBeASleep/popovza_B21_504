fb7b135f9a9d88b12367714617930d70b8dd1fa8
Zakhar <mix.cat@mail.ru>
2024-03-19 10:05:58 +0300

    3lab

--- 3sem/results/main.py
+++ 3sem/results/main.py
@@ -1,11 +1,11 @@
+import numpy as np
+from PIL import Image as pim
 from glob import glob
 import os
-from PIL import Image as pim
-import numpy as np
 
 def microchel(x, y, arr):
-    mat = (arr[y][x] + arr[y][x+1] + arr[y+1][x] + arr[y+1][x+1]) // 4
-    disp = (arr[y][x] - mat) ** 2 + (arr[y][x+1] - mat) ** 2 + (arr[y+1][x] - mat) ** 2 + (arr[y+1][x+1] - mat) ** 2
+    mat = np.sum(arr[y-1:y+1, x:x+2]) // 4
+    disp = (arr[y][x] - mat) ** 2 + (arr[y][x+1] - mat) ** 2 + (arr[y-1][x] - mat) ** 2 + (arr[y-1][x+1] - mat) ** 2
     return mat, disp
 
 def macrochel(x, y, arr):
@@ -26,23 +26,37 @@ def macrochel(x, y, arr):
         disp = tdisp
         mat = tmat
 
-    # return mat
-    return np.uint8(mat)
+    return mat
     
 
-def abc(imgname):
-    # inptArr = np.array(pim.open(imgname).convert('L'))
-    inptArr = np.array(pim.open(imgname).convert('L'), dtype=np.uint16)
+def plavycheeWinda(img):
+    inptArr = np.array(img)
     h, w = inptArr.shape
     newArr = np.zeros((h, w), dtype=np.uint8)
     for y in range (1, h-2):
         for x in range (1, w-2):
             newArr[y][x] = macrochel(x, y, inptArr)
     
-    return newArr
+    return pim.fromarray(newArr, mode='L')
+
+def toSRGB(imgname):
+    input_image = pim.open(imgname).convert('RGB')
+    origPicArr = np.array(input_image)
+    newPicArr = np.zeros((origPicArr.shape[0], origPicArr.shape[1]), dtype=origPicArr.dtype)
+    
+    for y in range(origPicArr.shape[0]):
+        for x in range(origPicArr.shape[1]):
+            newPicArr[y][x] = np.mean(origPicArr[y][x])
+
+    return pim.fromarray(newPicArr, mode='L')
 
 
-inpt = os.path.join(os.path.dirname(__file__), 'input/*')
-for name in glob(inpt):
-    ans = pim.fromarray(abc(name), mode='L')
-    ans.save(os.path.join(os.path.dirname(__file__), 'output', os.path.basename(name)))
+def main():
+    inptdir = os.path.join(os.path.dirname(__file__), 'input/*')
+    for filename in glob(inptdir):
+        polyTon = toSRGB(filename)
+        res = plavycheeWinda(polyTon)
+        res.save(os.path.join(os.path.dirname(__file__), 'output', os.path.splitext(os.path.basename(filename))[0] + '.bmp'))
+
+if __name__ == "__main__":
+    main()
